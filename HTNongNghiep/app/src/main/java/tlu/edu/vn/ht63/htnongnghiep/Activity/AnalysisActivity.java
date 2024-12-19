00ba24bdf4916c001402fe246df12b9b649d407c
thanhst <thanhxk2003@gmail.com>
2024-12-19 20:01:13 +0700

    update

--- HTNongNghiep/app/src/main/java/tlu/edu/vn/ht63/htnongnghiep/Activity/AnalysisActivity.java
+++ HTNongNghiep/app/src/main/java/tlu/edu/vn/ht63/htnongnghiep/Activity/AnalysisActivity.java
@@ -16,7 +16,7 @@ public class AnalysisActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.fragment_home);
+        setContentView(R.layout.fragment_home_test);
         if (savedInstanceState == null) {
             // Tạo một Fragment mới
             Fragment imageAnalysisFragment = new ImageAnalysis_view();
