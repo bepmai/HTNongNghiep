77fd17c48c07113edb0bb1cfe1398567930bb04f
bepmai <nguyenphgmaiii23@gmail.com>
2024-12-24 10:53:12 +0700

    update language

--- HTNongNghiep/app/src/main/java/tlu/edu/vn/ht63/htnongnghiep/Container/UI/Garden.java
+++ HTNongNghiep/app/src/main/java/tlu/edu/vn/ht63/htnongnghiep/Container/UI/Garden.java
@@ -1,5 +1,6 @@
 package tlu.edu.vn.ht63.htnongnghiep.Container.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import tlu.edu.vn.ht63.htnongnghiep.R;
 
@@ -25,20 +28,12 @@ public class Garden extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private FloatingActionButton btn_add;
 
     public Garden() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Garden.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Garden newInstance(String param1, String param2) {
         Garden fragment = new Garden();
         Bundle args = new Bundle();
@@ -61,6 +56,13 @@ public class Garden extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_garden, container, false);
+        View view = inflater.inflate(R.layout.fragment_garden, container, false);
+        btn_add = view.findViewById(R.id.btn_add);
+
+        btn_add.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), AddPlant.class);
+            startActivity(intent);
+        });
+        return view;
     }
 }
