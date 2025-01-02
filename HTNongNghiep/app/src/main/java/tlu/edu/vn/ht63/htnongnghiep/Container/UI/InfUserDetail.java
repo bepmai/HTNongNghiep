7c1e0e4e2f83d711adcbc94f3069365e1b3fbef8
bepmai <nguyenphgmaiii23@gmail.com>
2025-01-02 10:10:33 +0700

    update infor user

--- HTNongNghiep/app/src/main/java/tlu/edu/vn/ht63/htnongnghiep/Container/UI/InfUserDetail.java
+++ HTNongNghiep/app/src/main/java/tlu/edu/vn/ht63/htnongnghiep/Container/UI/InfUserDetail.java
@@ -2,16 +2,23 @@ package tlu.edu.vn.ht63.htnongnghiep.Container.UI;
 
 import android.app.DatePickerDialog;
 import java.util.Calendar;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.github.mikephil.charting.renderer.DataRenderer;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -20,8 +27,10 @@ import java.util.Locale;
 import tlu.edu.vn.ht63.htnongnghiep.R;
 
 public class InfUserDetail extends AppCompatActivity {
-    EditText input_birthday;
+    EditText input_name, input_email, input_nameplant, input_birthday;
     Spinner input_gender;
+    Button btn_update;
+    private DataRenderer dataRenderer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +42,14 @@ public class InfUserDetail extends AppCompatActivity {
                     v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
                     return insets;
             });
-
+        input_name = findViewById(R.id.input_name);
+        input_email = findViewById(R.id.input_email);
+        input_nameplant = findViewById(R.id.input_nameplant);
         input_birthday = findViewById(R.id.input_birthday);
         input_gender = findViewById(R.id.input_gender);
+        btn_update = findViewById(R.id.btn_update);
+
+        Intent intent = getIntent();
 
         input_birthday.setOnClickListener(view -> {
             Calendar calendar = Calendar.getInstance();
