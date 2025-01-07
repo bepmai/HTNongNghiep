e603c2d04e071d1c3b2d8e944401a1b6a38ab611
bepmai <nguyenphgmaiii23@gmail.com>
2025-01-07 22:16:05 +0700

    update store- product

--- HTNongNghiep/app/src/main/java/tlu/edu/vn/ht63/htnongnghiep/Adapter/PlantAdapter.java
+++ HTNongNghiep/app/src/main/java/tlu/edu/vn/ht63/htnongnghiep/Adapter/PlantAdapter.java
@@ -45,13 +45,13 @@ public class PlantAdapter extends RecyclerView.Adapter<PlantAdapter.PlantViewHol
                 .placeholder(R.drawable.group260) // Ảnh hiển thị khi đang tải
                 .error(R.drawable.group260)       // Ảnh hiển thị khi lỗi
                 .into(holder.image);
-        holder.id.setText(plant.getId());
-        holder.idPlant.setText(plant.getIdplant());
-        holder.idUser.setText(plant.getUserid());
+//        holder.id.setText(plant.getId());
+//        holder.idPlant.setText(plant.getIdplant());
+//        holder.idUser.setText(plant.getUserid());
         holder.nameUser.setText(plant.getNameuser());
-        SimpleDateFormat dateTimeFormat = new SimpleDateFormat("HH:mm dd/MM/yyyy");
-        holder.dateSell.setText(dateTimeFormat.format(plant.getDatesell()));
-        holder.address.setText(plant.getAddress());
+//        SimpleDateFormat dateTimeFormat = new SimpleDateFormat("HH:mm dd/MM/yyyy");
+//        holder.dateSell.setText(dateTimeFormat.format(plant.getDatesell()));
+//        holder.address.setText(plant.getAddress());
         holder.description.setText(plant.getDescription());
 
         // Thiết lập sự kiện click trực tiếp trong adapter
@@ -64,13 +64,12 @@ public class PlantAdapter extends RecyclerView.Adapter<PlantAdapter.PlantViewHol
             intent.putExtra("plant_price", plant.getPrice());
             intent.putExtra("plant_rating", plant.getRating());
             intent.putExtra("plant_image", plant.getImage());
-            intent.putExtra("plant_id", plant.getId());
-            intent.putExtra("plant_idplant", plant.getIdplant());
-            intent.putExtra("plant_iduser", plant.getUserid());
-            intent.putExtra("plant_nameuser", plant.getNameuser());
-            intent.putExtra("plant_datesell", plant.getDatesell());
-            intent.putExtra("plant_address", plant.getAddress());
             intent.putExtra("plant_description", plant.getDescription());
+            intent.putExtra("plant_nameuser", plant.getNameuser());
+//            intent.putExtra("plant_idplant", plant.getIdplant());
+//            intent.putExtra("plant_iduser", plant.getUserid());
+//            intent.putExtra("plant_datesell", plant.getDatesell());
+//            intent.putExtra("plant_address", plant.getAddress());
 
             // Start activity
             holder.itemView.getContext().startActivity(intent);
@@ -94,13 +93,13 @@ public class PlantAdapter extends RecyclerView.Adapter<PlantAdapter.PlantViewHol
             discount = itemView.findViewById(R.id.product_discount);
             rating = itemView.findViewById(R.id.product_rating);
             image = itemView.findViewById(R.id.product_image);
-            id = itemView.findViewById(R.id.product_id);
-            idPlant = itemView.findViewById(R.id.product_idplant);
-            idUser = itemView.findViewById(R.id.product_iduser);
             nameUser = itemView.findViewById(R.id.product_nameuser);
-            dateSell = itemView.findViewById(R.id.product_datesell);
-            address = itemView.findViewById(R.id.product_address);
             description = itemView.findViewById(R.id.product_description);
+//            id = itemView.findViewById(R.id.product_id);
+//            idPlant = itemView.findViewById(R.id.product_idplant);
+//            idUser = itemView.findViewById(R.id.product_iduser);
+//            dateSell = itemView.findViewById(R.id.product_datesell);
+//            address = itemView.findViewById(R.id.product_address);
         }
     }
 
